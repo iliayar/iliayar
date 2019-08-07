1384352ee1d8cdb89c2adef84ada28f837fe01a4
iliayar <iliayar3@gmail.com>
2019-08-08 00:30:13 +0300

    Hotfix 2

--- log.php
+++ log.php
@@ -2,7 +2,7 @@
 	if(!isset($_GET['path'])) {
 		exit(0);
 	}
-	$fp = fopen("/var/log/nginx/ips.log", "a");
+	$fp = fopen("./ips.log", "a");
 
 	$msg = "[".date("d.m.y H:i:s",time())."] ".$_SERVER['REMOTE_ADDR']." ".$_GET['path'];
 	fwrite($fp, $msg);
