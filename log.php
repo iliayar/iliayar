becb999596a2655449d4923bfe3f83e91ad6edde
iliayar <iliayar3@gmail.com>
2019-08-08 00:31:16 +0300

    Hotfix 3

--- log.php
+++ log.php
@@ -4,7 +4,7 @@
 	}
 	$fp = fopen("./ips.log", "a");
 
-	$msg = "[".date("d.m.y H:i:s",time())."] ".$_SERVER['REMOTE_ADDR']." ".$_GET['path'];
+	$msg = "[".date("d.m.y H:i:s",time())."] ".$_SERVER['REMOTE_ADDR']." ".$_GET['path']."\n";
 	fwrite($fp, $msg);
 	fclose($fp);
 ?>
