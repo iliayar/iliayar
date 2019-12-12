ce61364034c354d383b47988eb4e1fb016f65277
iliayar <iliayar3@gmail.com>
2019-12-12 21:11:54 +0300

    New index EEE

---
+++ js/edved.js
@@ -0,0 +1,2 @@
+//$('audio')[0].outerHTML = '<embed id="embed_player" src="media/megalovania.mp3" loop="true" autostart="true" hidden="true"></embed>';
+//$('#megalovania').outerHTML('<audio controls loop autoplay hidden><source src = "media/megalovania.mp3"></audio>');
--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-//$('audio')[0].outerHTML = '<embed id="embed_player" src="media/megalovania.mp3" loop="true" autostart="true" hidden="true"></embed>';
-//$('#megalovania').outerHTML('<audio controls loop autoplay hidden><source src = "media/megalovania.mp3"></audio>');
+jQuery(function(){
+    jQuery("#bgndVideo").YTPlayer();
+});
+
+$(document).ready(function() {
+    $('.playpause').click(function() {
+        state = jQuery("#bgndVideo").YTPGetPlayer().getPlayerState();
+        if(state != 1) {
+            jQuery("#bgndVideo").YTPPlay();
+        } else {
+            jQuery("#bgndVideo").YTPPause();
+        }
+    })
+})
