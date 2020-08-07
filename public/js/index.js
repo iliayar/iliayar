1e420100048464a3af66b145ec88f86a3cc270bd
Ilya <iliayar3@gmail.com>
2020-08-08 00:48:16 +0300

    blinking cursor

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,28 @@
+function cursorHide() {
+    jQuery("#cursor").hide();
+    setTimeout(cursorShow, 1000);
+}
+
+function cursorShow() {
+    jQuery("#cursor").show();
+    setTimeout(cursorHide, 1000);
+}
+
 jQuery(function(){
     jQuery("#bgndVideo").YTPlayer();
+    setTimeout(cursorHide, 1000);
 });
+
 
 var un_mute = jQuery("#un-mute")[0];
 
 un_mute.onclick = function() {
     player = jQuery("#bgndVideo");
-    mute = jQuery("#un-mute")[0];
+    mute = jQuery("#un-mute");
     if(player.YTPGetVolume() == 0) {
-	mute.innerHTML = "mute";
+	mute.html("mute");
     } else {
-	mute.innerHTML = "unmute";
+	mute.html("unmute");
     }
     player.YTPToggleVolume();
 };
