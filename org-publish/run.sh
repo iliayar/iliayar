87e86eda9ba23def7a4b1822c3809a777d71b67f
iliayar <iliayar3@gmail.com>
2022-04-10 02:37:05 +0300

    publish: remove emacs run scripts

--- org-publish/run.sh
+++
@@ -1,9 +0,0 @@
-
-docker run \
-    -i \
-    --rm \
-    -v "${WORKSPACE}/public/notes:/publish/input" \
-    -v "/var/www/mainsite/public/public-notes:/publish/output" \
-    -v "${WORKSPACE}/org-publish/cache:/root/.org-timestamps" \
-    -v "${WORKSPACE}/org-publish/config:/publish/config" \
-    org-publish
