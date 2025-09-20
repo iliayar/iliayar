9c7c96d92a2a6a668a10aca21420bbaaeca40c87
iliayar <iliayar3@gmail.com>
2025-09-20 17:23:47 +0300

    fix(conspects-builder): don't output latex log

--- builder/lib/src/Site/LatexCompiler.hs
+++ builder/lib/src/Site/LatexCompiler.hs
@@ -15,7 +15,7 @@ import System.Directory (copyFile, doesFileExist)
 import System.Exit (ExitCode)
 import System.FilePath.Posix (addExtension, takeBaseName, (</>))
 import System.IO.Temp (withSystemTempDirectory)
-import System.Process (spawnProcess, waitForProcess)
+import System.Process (StdStream (..), createProcess, proc, spawnProcess, std_err, std_out, waitForProcess)
 
 newtype Latex = Latex
   { tFrom :: FilePath
@@ -35,7 +35,12 @@ instance Exception LatexNoFileProduced
 instance Writable Latex where
   write dst (Item _ (Latex src)) = withSystemTempDirectory "hakyll-latex" $ \dir -> do
     -- NOTE(iliayar): Ignoring exit code of pdflatex, because it can be non 0, but pdf is produced
-    h <- spawnProcess "pdflatex" ["-shell-escape", "--synctex=1", "-interaction", "nonstopmode", "-output-directory", dir, src]
+    (_, _, _, h) <-
+      createProcess
+        (proc "pdflatex" ["-shell-escape", "--synctex=1", "-interaction", "nonstopmode", "-output-directory", dir, src])
+          { std_out = CreatePipe,
+            std_err = CreatePipe
+          }
     _ <- waitForProcess h
     let pdfFile = dir </> addExtension (takeBaseName src) "pdf"
     pdfFileExists <- doesFileExist pdfFile
